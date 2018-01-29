a5529c930783327a7e8fc1eefa49e3bacb16b7f6
Jan Rothen <jan.rothen@me.com>
2018-01-29 15:37:31 +0100

    added model and controller for wallet

---
+++ src/Lykke.Service.Stellar.Sign/Models/WalletModels.cs
@@ -0,0 +1,9 @@
+﻿namespace Lykke.Service.Stellar.Sign.Models
+{
+    public class WalletModels
+    {
+        public string PrivateKey { get; set; }
+
+        public string PublicAddress { get; set; }
+    }
+}
---
+++ src/Lykke.Service.Stellar.Sign/Models/WalletsController.cs
@@ -0,0 +1,30 @@
+﻿using Lykke.Service.Stellar.Sign.Core.Services;
+using Lykke.Service.Stellar.Sign.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+namespace Lykke.Service.Stellar.Sign.Models
+{
+    [Route("api/wallets")]
+    public class WalletsController : Controller
+    {
+        private readonly IStellarService _stellarService;
+
+        public WalletsController(IStellarService stellarService)
+        {
+            _stellarService = stellarService;
+        }
+
+        [HttpPost]
+        public WalletResponse Post()
+        {
+            var privateKey = _stellarService.GetPrivateKey();
+            var publicAddress = _stellarService.GetPublicAddress(privateKey);
+
+            return new WalletResponse()
+            {
+                PrivateKey = privateKey,
+                PublicAddress = publicAddress
+            };
+        }
+    }
+}
