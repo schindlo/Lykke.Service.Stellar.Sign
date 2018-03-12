6efb6fe2b43eaa2d17272172050b35ca5b1e8be0
Oliver Schindler <oliver@schindler.gdn>
2018-03-12 21:06:05 +0100

    fix sign controller validation

--- src/Lykke.Service.Stellar.Sign/Controllers/SignController.cs
+++ src/Lykke.Service.Stellar.Sign/Controllers/SignController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Lykke.Service.Stellar.Sign.Core.Services;
 using Lykke.Service.Stellar.Sign.Models;
+using Lykke.Common.Api.Contract.Responses;
 
 namespace Lykke.Service.Stellar.Sign.Controllers
 {
@@ -17,8 +18,18 @@ namespace Lykke.Service.Stellar.Sign.Controllers
 
         [HttpPost]
         [ProducesResponseType(typeof(SignResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int)HttpStatusCode.BadRequest)]
         public IActionResult SignTransaction([FromBody]SignRequest request)
         {
+            if (request.PrivateKeys == null || request.PrivateKeys.Length < 1)
+            {
+                return BadRequest(ErrorResponse.Create("Invalid parameter").AddModelError("request.privateKeys", "Must contain at least one item"));
+            }
+            if (string.IsNullOrWhiteSpace(request.TransactionContext))
+            {
+                return BadRequest(ErrorResponse.Create("Invalid parameter").AddModelError("request.transactionContext", "Must be non empty"));
+            }
+
             var xdrSigned = _stellarService.SignTransaction(request.PrivateKeys, request.TransactionContext);
             return Ok(new SignResponse
             {
