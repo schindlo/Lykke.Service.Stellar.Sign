5f551d167dcf2d1ad63eabc9dc6fb1b722acd999
Oliver Schindler <oliver@schindler.gdn>
2018-05-22 18:14:14 +0200

    remove TODOs from template generated classes

--- src/Lykke.Service.Stellar.Sign/Modules/ServiceModule.cs
+++ src/Lykke.Service.Stellar.Sign/Modules/ServiceModule.cs
@@ -21,12 +21,6 @@ namespace Lykke.Service.Stellar.Sign.Modules
 
         protected override void Load(ContainerBuilder builder)
         {
-            // TODO: Do not register entire settings in container, pass necessary settings to services which requires them
-            // ex:
-            //  builder.RegisterType<QuotesPublisher>()
-            //      .As<IQuotesPublisher>()
-            //      .WithParameter(TypedParameter.From(_settings.CurrentValue.QuotesPublication))
-
             builder.RegisterInstance(_log)
                 .As<ILog>()
                 .SingleInstance();
