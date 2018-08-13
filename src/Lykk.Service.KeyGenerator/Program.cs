3b28279253044cf992fdea6ea0f6527f89df2ea4
AleksandrBelousov <tortyt1@gmail.com>
2018-08-13 13:48:59 +0400

    Tools is added

---
+++ src/Lykk.Service.KeyGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lykk.Service.KeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stellarPrivateKeyPair = StellarBase.KeyPair.Random();
+            Console.WriteLine($"Seed(PrivateKey): {stellarPrivateKeyPair.Seed}");
+            Console.WriteLine($"Address(PublicAddress): {stellarPrivateKeyPair.Address}");
+            Console.ReadLine();
+        }
+    }
+}
